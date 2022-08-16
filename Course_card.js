cc267d6476dc477210d77ba2cdf8048ff25d4ede
Mohamed Badr <60277974+Mohamed-A-Badr@users.noreply.github.com>
2022-08-16 11:40:50 +0200

    bld.ai

---
+++ Course_card.js
@@ -0,0 +1,126 @@
+const container = document.querySelector(".courses-container");
+export const generateCards = ({
+  img,
+  title,
+  author,
+  total_rate,
+  no_of_ratings,
+  new_Price,
+  discount,
+}) => {
+  //create new div with class name card
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add("card");
+
+  //create new div to give space for the image
+  const cardImageContainer = document.createElement("div");
+  cardImageContainer.classList.add("card-image");
+
+  //insert the image
+  const cardImage = document.createElement("img");
+  cardImage.src = img;
+
+  //insert the image in the image container
+  cardImageContainer.appendChild(cardImage);
+  //insert image container inside card container
+  cardContainer.appendChild(cardImageContainer);
+
+  //create a new container for card content
+  const cardContentContainer = document.createElement("div");
+  cardContentContainer.classList.add("card-title");
+
+  //create h3 contain course name
+  const title = document.createElement("h3");
+  const titleText = document.createTextNode(title);
+  title.appendChild(titleText);
+
+  //insert it inside the card container
+  cardContentContainer.appendChild(title);
+
+  //create p contain author name
+  const author = document.createElement("p");
+  const authorName = document.createTextNode(author);
+  author.appendChild(authorName);
+
+  //insert it inside the card container
+  cardContentContainer.appendChild(author);
+
+  //creating a rating container for stars and total rate
+  const rateContainer = document.createElement("div");
+  rateContainer.classList.add("rating");
+
+  //create a span for the total rate
+  const totRate = document.createElement("span");
+  totRate.classList.add("total_rate");
+
+  //insert inside the span the value
+  const totRateText = document.createTextNode(total_rate);
+  totRate.appendChild(totRateText);
+
+  //push the total rate inside the rating container
+  rateContainer.appendChild(totRate);
+
+  //creating full Stars and push it inside the rating container
+  for (let i = 0; i < 4; i++) {
+    let fullStar = document.createElement("span");
+    fullStar.classList.add("fa fa-star checked");
+
+    rateContainer.appendChild(fullStar);
+  }
+  //create and push the half star inside the rating container
+  const halfStar = documnet.createElement("span");
+  halfStar.classList.add("fa fa-star-half-full");
+
+  rateContainer.appendChild(halfStar);
+
+  //creat a span for number of people gives rate
+  const people = document.createElement("span");
+  people.classList.add("no-of-ratings");
+
+  /*
+    intsert inside the span number of people and push it
+    and push it inside the rate container
+  */
+  const peopleText = document.createTextNode(no_of_ratings);
+  people.appendChild(peopleText);
+  rateContainer.appendChild(people);
+
+  //push the rate container inside the card title container
+  cardContentContainer.appendChild(rateContainer);
+  //then push it inside the cards container
+  cardContainer.appendChild(cardContentContainer);
+
+  //create price container
+  const priceContainer = document.createElement("div");
+  priceContainer.classList.add("course-price");
+
+  /*
+  create pahargraph element and insert inside it the 
+  new price
+  */
+  const newPrice = document.createElement("p");
+  newPrice.classList.add("new-price");
+  const newPriceText = document.createTextNode(new_Price);
+  newPrice.appendChild(newPriceText);
+
+  //push the pahargraph inside the price container
+  priceContainer.appendChile(newPrice);
+
+  /*
+  create pahargraph element and insert inside it the 
+  discount
+  */
+  const priceDiscount = document.createElement("p");
+  priceDiscount.classList.add("discount");
+  const priceDiscountText = document.createTextNode(discount);
+  priceDiscount.appendChild(priceDiscountText);
+
+  //push the pahargraph inside the price container
+  priceContainer.appendChild(priceDiscount);
+
+  //push the price container inside the card container
+  cardContainer.appendChild(priceContainer);
+
+  //push the card container inside the courses container
+  container.appendChild(cardContainer);
+};
---
+++ index.js
@@ -0,0 +1,15 @@
+import coursesCard from "./Courses_card.js";
+
+const fetchData = async () => {
+  const response = await fetch("http://localhost:3000/Courses");
+  return await response.json();
+};
+
+const udemyCourses = document.querySelector(".courses-container");
+if (udemyCourses) {
+  const udemyCoursesData = await fetchData();
+  const udemyCoursesDataArray = Array.from(udemyCoursesData);
+  udemyCoursesDataArray.forEach((item) => {
+    generateCards(item);
+  });
+}
