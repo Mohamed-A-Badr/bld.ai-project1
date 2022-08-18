f6b3eac6ccd6c887ba453c1f4746eb953f7d2271
Mohamed Badr <60277974+Mohamed-A-Badr@users.noreply.github.com>
2022-08-18 12:21:24 +0200

    bld.ai

--- fetch.js
+++ fetch.js
@@ -1,18 +1,43 @@
 import { generateCard } from "./Course_card.js";
 import { search } from "./Course_card.js";
 
-const courseType = document.querySelector("li");
+let indx = 0;
+const btn1 = document.getElementById("btn_python");
+const btn2 = document.getElementById("btn_excel");
+const btn3 = document.getElementById("btn_javascript");
+
+export function python() {
+  indx = 0;
+  btn1.style.color = "black";
+  btn2.style.color = "#8b8c8e";
+  btn3.style.color = "#8b8c8e";
+}
+export function excel() {
+  indx = 1;
+  btn2.style.color = "black";
+  btn1.style.color = "#8b8c8e";
+  btn3.style.color = "#8b8c8e";
+}
+export function javascript() {
+  indx = 2;
+  btn3.style.color = "black";
+  btn2.style.color = "#8b8c8e";
+  btn1.style.color = "#8b8c8e";
+}
 
 const fetchData = async () => {
-  const response = await fetch(`${courseType.getAttribute("href")}`);
+  const response = await fetch(`http://localhost:3000/Courses`);
   return await response.json();
 };
+console.log(indx);
 
 const udemyCourses = document.querySelector(".courses-container");
 if (udemyCourses) {
   const udemyCoursesData = await fetchData();
   const udemyCoursesDataArray = Array.from(udemyCoursesData);
-  udemyCoursesDataArray.forEach((item) => {
+  const Data = udemyCoursesDataArray[indx];
+  console.log(Data);
+  Data.forEach((item) => {
     generateCard(item);
   });
 }
